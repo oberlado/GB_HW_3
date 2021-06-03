bc46c5ceeb23b7da58ff0d8085dd4b3b051ba4fd
oberlado <78648733+oberlado@users.noreply.github.com>
2021-06-03 23:09:12 +0900

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,54 @@
+package less;
+
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Car implements Runnable {
+    private static AtomicInteger ai;
+    private static int CARS_COUNT;
+
+    static {
+        ai = new AtomicInteger(0);
+    }
+
+    private Race race;
+    private int speed;
+    private String name;
+    private CyclicBarrier barrier;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public Car(Race race, int speed, CyclicBarrier barrier) {
+        this.race = race;
+        this.speed = speed;
+        CARS_COUNT++;
+        this.name = "Участник #" + CARS_COUNT;
+        this.barrier = barrier;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int) (Math.random() * 800));
+            System.out.println(this.name + " готов");
+            barrier.await();
+            barrier.await();
+            for (int i = 0; i < race.getStages().size(); i++) {
+                race.getStages().get(i).go(this);
+            }
+            if (ai.incrementAndGet()==1){
+                System.out.println(name+"Win!");
+            }
+            barrier.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Tunnel.java
@@ -0,0 +1,28 @@
+package less;
+
+import java.util.concurrent.Semaphore;
+
+public class Tunnel extends Stage {
+    private Semaphore smp;
+    public Tunnel() {
+        smp= new Semaphore(Main.CARS_COUNT/2);
+        this.length = 80;
+        this.description = "Тоннель " + length + " метров";
+    }
+
+    @Override
+    public void go(Car c) {
+        try {if (!smp.tryAcquire()) {
+            System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+            smp.acquire();
+        }
+                System.out.println(c.getName() + " начал этап: " + description);
+                Thread.sleep(length / c.getSpeed() * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(c.getName() + " закончил этап: " + description);
+                smp.release();
+            }
+        }
+    }
